8ca18ccd4f8de25c987b0eb6402d640aa868af35
saki <39420706@qq.com>
2019-08-09 20:28:44 +0800

    使用vuex添加搜索记录的数据到本地存储
     // 组件读取store数据:this.$store.state
    // 把值存储到store中:使用方法 this.$store.commit('方法的文件名/方法名')
    
    // 通过vuex插件vuex-persistedstate，把数据存储到本地，本地的数据也会同步到store
    //读取本地数据:通过模块拿属性 (vuex模块化) this.$store.state.模块名(文件名).属性

--- plugins/localStorage.js
+++ plugins/localStorage.js
@@ -1,13 +1,9 @@
-// 这个是使用插入插件vuex-persistedstate`，该插件会把本地存储的数据读取到`store`。
-
 import createPersistedState from 'vuex-persistedstate'
 
-export default ({
-  store
-}) => {
+export default ({ store }) => {
   window.onNuxtReady(() => {
     createPersistedState({
-      key: "store", // 读取本地存储的数据到store
+      key: 'store'
     })(store)
   })
 }
